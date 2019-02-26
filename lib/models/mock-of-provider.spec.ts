2a4811179dda6a6e56dded441a39f12c9faf1466
Isaac Datlof <ike18t@gmail.com>
2019-02-25 21:05:24 -0500

    fix: don't spy on already spied functions

--- lib/models/mock-of-provider.spec.ts
+++ lib/models/mock-of-provider.spec.ts
@@ -14,6 +14,13 @@ describe('MockOfProvider', () => {
     expect(mock.getFoo).toHaveBeenCalled();
   });
 
+  it('does not spy on an already stubbed method', () => {
+    const mock = new MockOfProvider(class {}, {getFoo: jasmine.createSpy()}) as any;
+    mock.getFoo();
+
+    expect(mock.getFoo).toHaveBeenCalled();
+  });
+
   it('applies properties from stub to class instance', () => {
     const mock = new MockOfProvider(class {}, {myProp: 'my value'}) as any;
 
