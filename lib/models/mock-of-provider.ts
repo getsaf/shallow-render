471579307a42f419624c7b5d5e1e520c87b17188
Brandon Domingue <getsaf@gmail.com>
2019-09-08 17:14:50 -0400

    fix: Use TestBed.overrideProvider to support rootProviders

--- lib/models/mock-of-provider.ts
+++ lib/models/mock-of-provider.ts
@@ -1,8 +1,9 @@
-import { Provider } from '@angular/core';
+import { Type } from '@angular/core';
 import { testFramework } from '../test-framework';
+import { getProviderName } from '../tools/get-provider-name';
 
 export class MockOfProvider {
-  constructor(public mockOf: Provider, stubs: any = {}) {
+  constructor(public mockOf: Type<any>, stubs: any = {}) {
     Object.assign(this, stubs);
     Object.keys(stubs).forEach(key => {
       if (typeof (this as any)[key] === 'function' && !testFramework.isSpy((this as any)[key])) {
@@ -11,3 +12,13 @@ export class MockOfProvider {
     });
   }
 }
+
+export const mockProviderClass = <TProvider extends Type<any>>(provider: TProvider, stubs: any): TProvider => {
+  class MockProvider extends MockOfProvider { /* tslint:disable-line no-unnecessary-class */
+    constructor() {
+      super(provider, stubs);
+    }
+  }
+  Object.defineProperty(MockProvider, 'name', {value: `MockOf${getProviderName(provider)}`});
+  return MockProvider as TProvider;
+};
