0b6e24b885e40044c370483efa975d82c1a198bf
mateusz.gizycki <mateusz.gizycki@ocado.com>
2019-10-24 13:00:39 +0200

    added additional examples for forms and communication between components

---
+++ lib/examples/component-with-input.spec.ts
@@ -0,0 +1,49 @@
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Shallow } from '../shallow';
+
+////// Module Setup //////
+
+@Component({
+  selector: 'custom-form',
+  template: `
+      <input id="customInput" [(ngModel)]="text">
+  `,
+})
+class CustomFormComponent {
+  text = 'I am complete';
+}
+
+@NgModule({
+  imports: [FormsModule],
+  declarations: [CustomFormComponent]
+})
+class CustomInputModule {}
+//////////////////////////
+
+describe('component with input', () => {
+  let shallow: Shallow<CustomFormComponent>;
+
+  beforeEach(() => {
+    shallow = new Shallow(CustomFormComponent, CustomInputModule);
+  });
+
+  it('displays an input with default value', async () => {
+    const {find} = await shallow.render();
+
+    expect(find('#customInput').nativeElement.value)
+      .toBe('I am complete');
+  });
+
+  it('input updates ngModel', async () => {
+    const {find, instance} = await shallow.render();
+
+    const input = find('#customInput');
+    input.nativeElement.value = 'It works!';
+    input.triggerEventHandler('input', {target: input.nativeElement});
+
+    expect(instance.text)
+      .toBe('It works!');
+  });
+
+});
