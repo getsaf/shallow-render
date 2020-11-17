d3d3afe43608f1a9009bd3c63ef9150bff65dbbf
Brandon Domingue <getsaf@gmail.com>
2020-11-17 13:26:39 -0500

    Fix tests remove accidental buld folder

--- lib/examples/using-replacement-modules.spec.ts
+++ lib/examples/using-replacement-modules.spec.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { Component, NgModule, Type } from '@angular/core';
-import { fakeAsync, tick } from '@angular/core/testing';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { fakeAsync, flush } from '@angular/core/testing';
 import { Shallow } from '../shallow';
 
 ////// Module Setup //////
@@ -9,17 +9,15 @@ import { Shallow } from '../shallow';
   selector: 'service-response-label',
   template: '<label>{{labelText}}</label>',
 })
-class FooLabelComponent {
+class FooLabelComponent implements OnInit {
   labelText?: string;
 
-  constructor(httpClient: HttpClient) {
-    httpClient
+  constructor(private _httpClient: HttpClient) {}
+  async ngOnInit() {
+    this.labelText = await this._httpClient
       .get<string>('/foo/as/a/service')
       .toPromise()
-      .then(
-        response => (this.labelText = response),
-        () => (this.labelText = 'ERROR')
-      );
+      .catch(() => 'ERROR');
   }
 }
 
@@ -37,23 +35,25 @@ describe('using replaceModule', () => {
     shallow = new Shallow(FooLabelComponent, FooLabelModule).replaceModule(HttpClientModule, HttpClientTestingModule);
   });
 
-  it('displays the response from the foo service', fakeAsync(async () => {
-    const { element, inject, fixture } = await shallow.render();
-    const client = inject(HttpTestingController);
-    client.expectOne('/foo/as/a/service').flush('foo response');
-    tick();
-    fixture.detectChanges();
+  it('displays the response from the foo service', fakeAsync(() => {
+    shallow.render().then(async ({ element, inject, fixture }) => {
+      const client = inject(HttpTestingController);
+      client.expectOne('/foo/as/a/service').flush('foo response');
+      flush();
+      fixture.detectChanges();
 
-    expect(element.nativeElement.innerText).toBe('foo response');
+      expect(element.nativeElement.innerText).toBe('foo response');
+    });
   }));
 
-  it('displays ERROR when a service error occurs', fakeAsync(async () => {
-    const { element, inject, fixture } = await shallow.render();
-    const client = inject(HttpTestingController);
-    client.expectOne('/foo/as/a/service').error(new ErrorEvent('BOOM'));
-    tick();
-    fixture.detectChanges();
+  it('displays ERROR when a service error occurs', fakeAsync(() => {
+    shallow.render().then(async ({ element, inject, fixture }) => {
+      const client = inject(HttpTestingController);
+      client.expectOne('/foo/as/a/service').error(new ErrorEvent('BOOM'));
+      flush();
+      fixture.detectChanges();
 
-    expect(element.nativeElement.innerText).toBe('ERROR');
+      expect(element.nativeElement.innerText).toBe('ERROR');
+    });
   }));
 });
