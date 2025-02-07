d24ea68f1bc2a3d5e67d359ef5d073d94160b219
Jasmin Ebner <jasmin.ebner@kwsoft.ch>
2025-02-07 16:19:05 +0100

    feat: support input signals
    - extend type
    - extend tests

--- lib/examples/component-with-bindings.spec.ts
+++ lib/examples/component-with-bindings.spec.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, NgModule, OnChanges, Output } from '@angular/core';
+import { Component, EventEmitter, input, Input,NgModule, OnChanges, Output } from '@angular/core';
 import { Shallow } from '../shallow';
 
 ////// Module Setup //////
@@ -14,11 +14,15 @@ interface Person {
     <label id="personLabel" (click)="selected.emit(person)">
       {{ person.firstName }} {{ person.lastName }} was born in {{ person.birthDate.getFullYear() }}
     </label>
+    <label id="partnerLabel" (click)="selected.emit(partner())">
+      {{ partner().firstName }} {{ partner().lastName }} was born in {{ partner().birthDate.getFullYear() }}
+    </label>
     <label id="ngOnChangesCount">{{ ngOnChangesCount }}</label>
   `,
 })
 class BornInComponent implements OnChanges {
   @Input({ required: true }) person!: Person;
+  partner =  input.required<Person>();
   @Output() selected = new EventEmitter<Person>();
 
   public ngOnChangesCount = 0;
@@ -47,21 +51,35 @@ describe('component with bindings', () => {
     birthDate: new Date('1982-05-11'),
   };
 
+  const partner: Person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    birthDate: new Date('1990-01-12'),
+  };
+
   it('displays the name and year the person was born', async () => {
-    const { find } = await shallow.render({ bind: { person } });
+    const { find } = await shallow.render({ bind: { person, partner } });
 
     expect(find('#personLabel').nativeElement.textContent).toContain('Brandon Domingue was born in 1982');
+    expect(find('#partnerLabel').nativeElement.textContent).toContain('John Doe was born in 1990');
   });
 
   it('emits the person when clicked', async () => {
-    const { find, outputs } = await shallow.render({ bind: { person } });
+    const { find, outputs } = await shallow.render({ bind: { person, partner }});
     find('#personLabel').nativeElement.click();
 
     expect(outputs.selected.emit).toHaveBeenCalledWith(person);
   });
 
+  it('emits the partner when clicked', async () => {
+    const { find, outputs } = await shallow.render({ bind: { person, partner }});
+    find('#partnerLabel').nativeElement.click();
+
+    expect(outputs.selected.emit).toHaveBeenCalledWith(partner);
+  });
+
   it('displays the number of times the person was updated', async () => {
-    const { find, fixture, bindings } = await shallow.render({ bind: { person } });
+    const { find, fixture, bindings } = await shallow.render({ bind: { person, partner }});
 
     expect(find('#ngOnChangesCount').nativeElement.textContent).toBe('1');
 
@@ -70,4 +88,10 @@ describe('component with bindings', () => {
 
     expect(find('#ngOnChangesCount').nativeElement.textContent).toBe('2');
   });
+
+  it('throws for missing required input signal', async  () => {
+    await expect(shallow.render({ bind: { person } }))
+      .rejects.toThrow('Input is required but no value is available yet.');
+  });
+
 });
--- lib/models/recursive-partial.ts
+++ lib/models/recursive-partial.ts
@@ -2,10 +2,15 @@
  * Utility type that converts an object to recursively have optional properties.
  * This includes items an arrays and return values for functions.
  */
+import { InputSignal } from '@angular/core';
+
 export type RecursivePartial<T> = Partial<{
-  [key in keyof T]: T[key] extends (...a: Array<infer U>) => any
-    ? (...a: Array<U>) => RecursivePartial<ReturnType<T[key]>> | ReturnType<T[key]>
-    : T[key] extends Array<any>
-      ? Array<RecursivePartial<T[key][number]>>
-      : RecursivePartial<T[key]> | T[key];
+  [key in keyof T]:
+  T[key] extends InputSignal<infer U>  // Handle signals like input() and input.required()
+    ? RecursivePartial<U>  // Extract the type and recursively apply RecursivePartial
+    : T[key] extends (...a: Array<infer U>) => any // Function-based properties (like methods or other signals)
+      ? (...a: Array<U>) => RecursivePartial<ReturnType<T[key]>> | ReturnType<T[key]>
+      : T[key] extends Array<any> // Handle array types
+        ? Array<RecursivePartial<T[key][number]>>
+        : RecursivePartial<T[key]> | T[key]; // Handle other standard cases
 }>;
